2f3af48ba9111083a0ba18890632ea5ba061250e
shrestic <nhatphongcgp@gmail.com>
2024-04-22 15:38:53 +0700

    init skeleton

---
+++ orchestrate/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="orchestrate",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "dagster",
+        "dagster-cloud",
+        "dagster-dbt",
+        "dbt-snowflake",
+    ],
+    extras_require={
+        "dev": [
+            "dagster-webserver",
+        ]
+    },
+)
