6cad14f04d1f1c7136b969b133734f93d70bd96c
shrestic <nhatphongcgp@gmail.com>
2024-05-03 10:44:11 +0700

    fixed schema

--- transform/models/staging/stg_order_items.sql
+++ transform/models/staging/stg_order_items.sql
@@ -11,25 +11,6 @@ order_items as (
         shipping_limit_date :: timestamp as shipping_limit_date, 
         price :: float as price, 
         freight_value :: float as freight_value,        
-        row_number() over(
-            partition by order_id
-            order by shipping_limit_date desc
-        ) as rw_num
     from source
 )
-select
-    order_id_key,
-    order_item_id_key, 
-    product_id_key, 
-    seller_id_key, 
-    shipping_limit_date, 
-    price, 
-    freight_value,    
-from order_items
-where (order_id_key, rw_num) in (
-        select 
-        order_id_key,
-        min(rw_num)
-        from order_items
-        group by order_id_key
-) 
+select * from order_items
